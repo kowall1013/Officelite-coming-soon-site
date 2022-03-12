650018ceff3e6d44c823ceafd751576c4747f9e4
mateusz.kowalski <mateusz.kowalski@trustmate.io>
2022-03-12 09:12:09 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import GlobalStyles from './globalStyles';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+`;
+
+function App(): JSX.Element {
+  return (
+    <Wrapper>
+      Matuś
+      <GlobalStyles />
+    </Wrapper>
+  );
+}
+
+export default App;
